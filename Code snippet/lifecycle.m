ebbaa2a653e17de2762541c4f3e6f7fd8071a2ef
jwr <jwr@nojeong-uui-MacBook.local>
2016-11-20 01:15:13 +0900

    lifecycle

---
+++ Code snippet/lifecycle.m
@@ -0,0 +1,38 @@
+---
+title: "UIViewController Lifecycle"
+summary: "placeholders for all of the view controller lifecycle methods"
+platform: iOS
+completion-scope: Class Implementation
+---
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
