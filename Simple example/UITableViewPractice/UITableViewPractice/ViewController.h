109f0df9091237a2d1bea153955b0b7728a03a8d
jwr <jwr@nojeong-uui-MacBook.local>
2016-11-21 11:51:03 +0900

    add TableViewPractice

---
+++ Simple example/UITableViewPractice/UITableViewPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewPractice
+//
+//  Created by 노정우 on 2016. 11. 21..
+//  Copyright © 2016년 jenny0616. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
